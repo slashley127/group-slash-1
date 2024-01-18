905e27c335fc9719b9bbbef50863c5324cf0152a
Sloane Wolter <sloanewolter@gmail.com>
2024-01-18 14:18:20 -0600

    filled out create portion of Activity Controller

--- src/main/java/org/launchcode/KidVenture/controllers/ActivityController.java
+++ src/main/java/org/launchcode/KidVenture/controllers/ActivityController.java
@@ -1,4 +1,44 @@
 package org.launchcode.KidVenture.controllers;
 
+
+import jakarta.validation.Valid;
+import org.launchcode.KidVenture.models.Activity;
+import org.launchcode.KidVenture.models.TypeOfActivity;
+import org.launchcode.KidVenture.models.data.ActivityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("kidVenture")
 public class ActivityController {
+
+    @Autowired
+    private ActivityRepository activityRepository;
+
+    @GetMapping("add")
+    public String displayAddEmployerForm(Model model) {
+        model.addAttribute("title", "Add Activity");
+        model.addAttribute(new Activity());
+        model.addAttribute("types", TypeOfActivity.values());
+        return "activities/create";
+    }
+
+    @PostMapping("add")
+    public String processAddEmployerForm(@ModelAttribute @Valid Activity newActivity,
+                                         Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            return "activites/create";
+        }
+        activityRepository.save(newActivity);
+        return "redirect:../";
+    }
+
 }
+
