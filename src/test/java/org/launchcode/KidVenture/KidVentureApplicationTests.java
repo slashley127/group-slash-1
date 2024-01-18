532e4af1a0a237f275805cb81b1b8af58d117905
Sloane Wolter <sloanewolter@gmail.com>
2024-01-18 13:22:09 -0600

    initialized springboot project

---
+++ src/test/java/org/launchcode/KidVenture/KidVentureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.KidVenture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidVentureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
