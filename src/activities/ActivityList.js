b676b6277201e8ac1f4e1d0119656526315a2e26
mollsilita <mollyvb14@yahoo.com>
2024-01-24 15:36:32 -0600

    added dummy data for activities

--- src/activities/ActivityList.js
+++ src/activities/ActivityList.js
@@ -1,70 +1,109 @@
+import { toHaveDisplayValue } from '@testing-library/jest-dom/matchers';
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { Button, ButtonGroup } from 'reactstrap';
-
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 class ActivityList extends Component {
   state = {
-    activities: []
-  };
+    activities: [
+      {
+        id: 1,
+        name: "Xyz",
+        child: "Oliver",
+        month: "Septemeber",
+        day: "Friday",
+        year: "2024",
+        typeOfActivity: "Coloring",
+        durationOfActivity: 20,
+        mood: "calm",
+
+
+
+        id: 2,
+        name: "Xyz",
+        child: "Alex",
+        month: "May",
+        day: "Saturday",
+        year: "2023",
+        typeOfActivity: "Spelling",
+        durationOfActivity: 30,
+        mood: "happy",
+
+
+
+        id: 3,
+        name: "Xyz",
+        child: "Molly",
+        month: "October",
+        day: "Sunday",
+        year: "2022",
+        typeOfActivity: "Watching TV",
+        durationOfActivity: 40,
+        mood: "excited",
+      }]
+  }
+
 
   async componentDidMount() {
     const response = await fetch("/api/activities");
     const body = await response.json();
-    this.setState({activities: body});
+    this.setState({ activities: body });
   }
 
   async remove(id) {
     await fetch(`/api/activities/${id}`, {
-        method: 'DELETE',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
     }).then(() => {
-        let updatedActivities = [...this.state.activities].filter(i => i.id !== id);
-        this.setState({activities: updatedActivities});
+      let updatedActivities = [...this.state.activities].filter(i => i.id !== id);
+      this.setState({ activities: updatedActivities });
     });
-}
+  }
 
   render() {
-    const {activities} = this.state;
+    const { activities } = this.state;
+
+
     return (
-        <div className="App">
-          <header className="App-header">
-            <div className="App-intro">
-              <h2>Activities</h2>
-                    <table>
-                      <tr>
-                        <th>Name of Activity</th>
-                        <th>Child's Name</th>
-                        <th>Date of Activity</th>
-                        <th>Type of Activity</th>
-                        <th>Duration of Activity</th>
-                        <th>Mood of Child</th>
-                        <th>Action</th>
-                       </tr>
-                       
-                        {activities.map(activity =>
-                          <tr key={activity.id}>
-                            <td>{activity.name}</td>
-                            <td>{activity.child}</td>
-                            <td>{activity.month}/{activity.day}/{activity.year}</td>
-                            <td>{activity.typeOfActivity}</td>
-                            <td>{activity.durationOfActivity}</td>
-                            <td>{activity.mood}</td>
-                            <td>
-                            <ButtonGroup>
-                                <Button size="sm" color="primary" tag={Link} to={"/api/activities/" + activity.id}>Edit</Button>
-                                <Button size="sm" color="danger" onClick={() => this.remove(activity.id)}>Delete</Button>
-                            </ButtonGroup>
-                            </td>
-                       </tr>
-                        )}
-                    </table>
-                  </div>
-          </header>
-        </div>
+      <div className="App">
+        <header className="App-header">
+          <div className="App-intro">
+            <h2>Activities</h2>
+            <table>
+              <tr>
+                <th>Name of Activity</th>
+                <th>Child's Name</th>
+                <th>Date of Activity</th>
+                <th>Type of Activity</th>
+                <th>Duration of Activity</th>
+                <th>Mood of Child</th>
+                <th>Action</th>
+              </tr>
+
+              {activities.map(activity =>
+                <tr key={activity.id}>
+                  <td>{activity.name}</td>
+                  <td>{activity.child}</td>
+                  <td>{activity.month}/{activity.day}/{activity.year}</td>
+                  <td>{activity.typeOfActivity}</td>
+                  <td>{activity.durationOfActivity}</td>
+                  <td>{activity.mood}</td>
+                  <td>
+                    <ButtonGroup>
+                      <Button size="sm" color="primary" tag={Link} to={"/api/activities/" + activity.id}>Edit</Button>
+                      <Button size="sm" color="danger" onClick={() => this.remove(activity.id)}>Delete</Button>
+                    </ButtonGroup>
+                  </td>
+                </tr>
+              )}
+            </table>
+          </div>
+        </header>
+      </div>
     );
   }
 }
