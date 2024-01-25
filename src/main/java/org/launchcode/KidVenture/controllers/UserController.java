325bb5105e1ed83cfb61ad47516735c4dc3413d5
effab <abeffan@gmail.com>
2024-01-24 19:34:23 -0600

    test

--- src/main/java/org/launchcode/KidVenture/controllers/UserController.java
+++ src/main/java/org/launchcode/KidVenture/controllers/UserController.java
@@ -1,14 +1,23 @@
 package org.launchcode.KidVenture.controllers;
 
+import org.launchcode.KidVenture.models.User;
 import org.launchcode.KidVenture.models.data.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("user")
 public class UserController {
     private UserRepository userRepository;
     public UserController(UserRepository userRepository){
         this.userRepository = userRepository;
     }
 
+    @GetMapping("{id")
+    public User getUser(@PathVariable Long id){
+        return userRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
 
 }
--- src/main/java/org/launchcode/KidVenture/models/User.java
+++ src/main/java/org/launchcode/KidVenture/models/User.java
@@ -1,12 +1,18 @@
 package org.launchcode.KidVenture.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Entity
 public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
 
     @NotNull(message = "Username is required.")
     @Size(min = 1, message = "Username is required")
@@ -32,6 +38,14 @@ public class User {
         this.password = password;
     }
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getUsername() {
         return username;
