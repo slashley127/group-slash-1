bd9f110468d23208e13d72a61af528a01a47431f
effab <abeffan@gmail.com>
2024-01-24 21:01:22 -0600

    test

--- src/main/java/org/launchcode/KidVenture/controllers/UserController.java
+++ src/main/java/org/launchcode/KidVenture/controllers/UserController.java
@@ -1,11 +1,13 @@
 package org.launchcode.KidVenture.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.KidVenture.models.User;
 import org.launchcode.KidVenture.models.data.UserRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("user")
@@ -15,9 +17,31 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-    @GetMapping("{id")
-    public User getUser(@PathVariable Long id){
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable int id){
         return userRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+    @PostMapping
+    public ResponseEntity createUser(@RequestBody User user) throws URISyntaxException{
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.created(new URI("/users/" + savedUser.getId())).body(savedUser);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateUser(@PathVariable int id, @RequestBody User user){
+        User currectUser = userRepository.findById(id).orElseThrow(RuntimeException::new);
+        currectUser.setUsername(user.getUsername());
+        currectUser.setEmail(user.getEmail());
+        currectUser.setPassword(user.getPassword());
+        currectUser = userRepository.save(user);
+        return ResponseEntity.ok(currectUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteUser(@PathVariable int id){
+        userRepository.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 
 }
--- src/main/java/org/launchcode/KidVenture/models/User.java
+++ src/main/java/org/launchcode/KidVenture/models/User.java
@@ -12,7 +12,7 @@ public class User {
 
     @Id
     @GeneratedValue
-    private Long id;
+    private int id;
 
     @NotNull(message = "Username is required.")
     @Size(min = 1, message = "Username is required")
@@ -39,11 +39,11 @@ public class User {
     }
 
 
-    public Long getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(int id) {
         this.id = id;
     }
 
