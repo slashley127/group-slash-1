a9a6ea672ea3baa502dccb58c64d55138b8185ba
effab <abeffan@gmail.com>
2024-01-27 20:41:04 -0600

    changes

--- src/main/java/org/launchcode/KidVenture/controllers/UserController.java
+++ src/main/java/org/launchcode/KidVenture/controllers/UserController.java
@@ -3,13 +3,14 @@ package org.launchcode.KidVenture.controllers;
 import org.launchcode.KidVenture.models.User;
 import org.launchcode.KidVenture.models.data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
@@ -17,5 +18,10 @@ public class UserController {
     @PostMapping("/user")
     User newUser(@RequestBody User newUser) {
         return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
