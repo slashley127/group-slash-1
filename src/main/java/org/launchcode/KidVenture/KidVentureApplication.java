eae1a53d3d409dbcf8047311966eee497581d8d8
effab <abeffan@gmail.com>
2024-02-03 15:51:18 -0600

    resolving issues

---
+++ src/main/java/org/launchcode/KidVenture/KidVentureApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.KidVenture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidVentureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidVentureApplication.class, args);
+	}
+
+}
