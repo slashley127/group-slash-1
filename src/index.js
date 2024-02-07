2eff7199bbc17690eed29bb9e3308b70fbd0f90b
Cookie Van <cookievan03@gmail.com>
2024-02-07 10:41:09 -0600

    changed index.js to updated syntax for v6 of react-dom

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
+import App from "./App";
 
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* Imported BrowswerRouter to make links in navigation bar work */}
-    <BrowserRouter>
+  <StrictMode>
     <App />
-    </BrowserRouter> 
-  </React.StrictMode>
+  </StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
