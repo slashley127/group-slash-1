050b811e50cd807eeaa76b789722a611b3a5f5d2
effab <abeffan@gmail.com>
2024-02-12 17:50:05 -0600

    adding

--- child-profile-front/src/App.js
+++ child-profile-front/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import React, { Component } from 'react';
+import ProfileInput from './Components/ProfileInput';
+import Profile from './Components/Profile';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <Router>
+          <Routes>
+            <Route path='/' exact={true} element={<ProfileInput />} />
+            <Route path='/profile' exact={true} element={<Profile />}/>
+          </Routes>
+        </Router>
+      </div>
+    )
+  }
+
+
 }
+
 
 export default App;
---
+++ child-profile-front/src/Components/ProfileInput.js
@@ -0,0 +1,108 @@
+import React, { Component } from "react";
+import { Container, Label, Form, FormGroup, Input, Button } from "reactstrap";
+import { useParams } from "react-router-dom";
+import goNavigate from "./Navigate";
+
+
+
+
+function withParams(Component) {
+    return props =>
+    <Component {...props} params={useParams()} />;
+}
+
+
+class ProfileInput extends Component{
+
+
+    emptyItem = {
+        childName:"",
+        dateOfBirth:""
+    };
+
+
+    constructor(props) {
+        super(props);
+        this.state={item: this.emptyItem};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    };
+
+
+    async componentDidMount() {
+        const {id} = this.props.params;
+        if (id !== 'new') {
+            const child = await(await fetch(`/child/${id}`)).json();
+            this.setState({item: child});
+        }
+    };
+
+
+    handleChange(e) {
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+        let item = {...this.state.item};
+        item[name] = value;
+        this.setState({item});
+    };
+
+
+    async handleSubmit(e) {
+        e.preventDefault();
+        const {item} = this.state;
+        if (!item.id) {
+            await fetch('/child', {
+                method: "POST",
+                headers: {
+                    'Accept': "application/json",
+                    'Content-Type': "application/json"
+                },
+                body: JSON.stringify(item)
+            });
+        } else {
+            await fetch(`/child/${item.id}`, {
+                method: "PUT",
+                headers: {
+                    'Accept': "application/json",
+                    'Content-Type': "application/json"
+                },
+                body: JSON.stringify(item)
+            });
+        }
+        this.props.navigation("/profile");
+    }
+
+
+    render() {
+        const {item} = this.state;
+        const title = <h2>Child Profile</h2>;
+
+
+        return<div>
+            <Container>
+                {title}
+                <Form onSubmit={this.handleSubmit}>
+                    <FormGroup>
+                        <Label for="childName">Name</Label>
+                        <Input type="text" name="childName" id="childName" value={item.childName || ''}
+                        onChange={this.handleChange} autoComplete="childName"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="dateOfBirth">Date Of Birth</Label>
+                        <Input type="text" name="dateOfBirth" id="dateOfBirth" value={item.dateOfBirth || ''}
+                        onChange={this.handleChange} placeholder="MM/DD/YYYY"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Button color="primary" type="submit">Submit</Button>
+                    </FormGroup>
+                </Form>
+            </Container>
+        </div>
+    }
+
+
+}
+
+
+export default withParams(goNavigate(ProfileInput));
