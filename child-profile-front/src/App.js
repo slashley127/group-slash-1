b63b2ff54b5c69988d1a2f782187a85da3fa92d5
effab <abeffan@gmail.com>
2024-02-12 18:16:42 -0600

    Revert "Child controller and changes"
    This reverts commit e7a1014b99ea52b7c2c738031336112d1b3b4669.

--- child-profile-front/src/App.js
+++ child-profile-front/src/App.js
@@ -1,28 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import React, { Component } from 'react';
-import ProfileInput from './Components/ProfileInput';
-import Profile from './Components/Profile';
 
-
-
-
-class App extends Component {
-  render(){
-    return(
-      <div>
-        <Router>
-          <Routes>
-            <Route path='/' exact={true} element={<ProfileInput />} />
-            <Route path='/profile' exact={true} element={<Profile />}/>
-          </Routes>
-        </Router>
-      </div>
-    )
-  }
-
-
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
-
 
 export default App;
