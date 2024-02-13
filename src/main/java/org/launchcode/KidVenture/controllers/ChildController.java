f24b8844ffbf9f33756b7879763940afabf5b7a9
effab <abeffan@gmail.com>
2024-02-12 19:06:22 -0600

    fixing

---
+++ src/main/java/org/launchcode/KidVenture/controllers/ChildController.java
@@ -0,0 +1,67 @@
+package org.launchcode.KidVenture.controllers;
+
+
+
+
+import org.apache.coyote.Response;
+import org.launchcode.KidVenture.models.Child;
+import org.launchcode.KidVenture.models.data.ChildRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/child")
+public class ChildController {
+
+
+    private final ChildRepository childRepository;
+
+
+    public ChildController(ChildRepository childRepository){
+        this.childRepository = childRepository;
+    }
+
+
+    @GetMapping
+    public List<Child> getChildren(){
+        return childRepository.findAll();
+    }
+
+
+    @GetMapping("/{id}")
+    public Child getChild(@PathVariable int id){
+        return childRepository.findById(id).orElseThrow(RuntimeException::new);
+    }
+
+
+    @PostMapping
+    public ResponseEntity createChild(@RequestBody Child child) throws URISyntaxException {
+        Child savedChild = childRepository.save(child);
+        return ResponseEntity.created(new URI("/child/" + savedChild.getId())).body(savedChild);
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity updateChild(@PathVariable int id, @RequestBody Child child){
+        Child currentChild = childRepository.findById(id).orElseThrow(RuntimeException::new);
+        currentChild.setChildName(child.getChildName());
+        currentChild.setDateOfBirth(child.getDateOfBirth());
+        currentChild = childRepository.save(child);
+
+
+        return ResponseEntity.ok(currentChild);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteChild(@PathVariable int id){
+        childRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/org/launchcode/KidVenture/models/data/ChildRepository.java
@@ -0,0 +1,9 @@
+package org.launchcode.KidVenture.models.data;
+
+
+import org.launchcode.KidVenture.models.Child;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ChildRepository extends JpaRepository<Child, Integer> {
+}
