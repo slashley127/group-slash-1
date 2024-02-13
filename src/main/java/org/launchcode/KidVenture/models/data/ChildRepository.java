0f1a52235f72c84300996f89da227a27cbebfa1a
effab <abeffan@gmail.com>
2024-02-12 18:01:37 -0600

    revert back

--- src/main/java/org/launchcode/KidVenture/models/data/ChildRepository.java
+++
@@ -1,7 +0,0 @@
-package org.launchcode.KidVenture.models.data;
-
-import org.launchcode.KidVenture.models.Child;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChildRepository extends JpaRepository<Child, Integer> {
-}
