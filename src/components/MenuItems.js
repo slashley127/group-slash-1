53090d525d0e9e81c3531013bd87515b0fddb44c
Cookie Van <cookievan03@gmail.com>
2024-02-18 17:03:25 -0600

    added updated navbar

---
+++ src/components/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+      title: 'Activities',
+      path: '/activities',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'Add Activity',
+      path: '/activities/new',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'Activity Generator',
+      path: '/enrichment/random',
+      cName: 'dropdown-link'
+    }
+  ];
